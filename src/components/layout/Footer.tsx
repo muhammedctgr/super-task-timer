2418fbaf15dbe54959129cbad88a997fa6bda876
greazely <muhammedko420@gmail.com>
2022-10-15 22:41:02 +0100

    initialcommit

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import { SITE_TITLE } from "../../constants/constants"
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 text-white text-center py-4">
+      <p className="mb-1">{SITE_TITLE} &copy;</p>
+      <p>Designed & developed by Popoola Muhammed</p>
+    </footer>
+  )
+}
+
+export default Footer
