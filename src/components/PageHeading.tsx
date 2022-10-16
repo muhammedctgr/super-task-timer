8bcae35a1ef89d75c7deb5ccf6eda25760919858
greazely <muhammedko420@gmail.com>
2022-10-16 12:26:45 +0100

    initialcommit

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,11 +1,12 @@
 interface Props {
-    extraClasses: string
-  }
-  
-  const PageHeading: React.FC<Props> = ({ children, extraClasses }) => {
-    const classes = "text-4xl text-green-900 font-semibold " + extraClasses
-  
-    return <h1 className={classes}>{children}</h1>
-  }
-  
-  export default PageHeading
+  extraClasses?: string;
+  children: React.ReactNode;
+}
+
+const PageHeading: React.FC<Props> = ({ children, extraClasses }) => {
+  const classes = 'text-4xl text-green-900 font-semibold ' + extraClasses;
+
+  return <h1 className={classes}>{children}</h1>;
+};
+
+export default PageHeading;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,21 @@
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout: React.FC = ({ children }) => {
+type Props = {
+  children: React.ReactNode;
+};
+
+
+const Layout: React.FunctionComponent<Props> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className='min-h-screen flex flex-col'>
       <Navbar />
-      <main className="flex flex-col grow">{children}</main>
-      <div className="mt-auto">
+      <main className='flex flex-col grow'>{children}</main>
+      <div className='mt-auto'>
         <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
